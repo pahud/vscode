04490b4e89abf589e60835f745f568febc0c1b16
Pahud Hsieh <pahudnet@gmail.com>
2020-11-03 12:18:22 +0000

    minor

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,8 @@ USERNAME=${USERNAME-vscode}
 # if in codespaces
 if [[ $CODESPACES == 'true' ]]; then
   printf 'ZSH_THEME="clean"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
+  # change 'docker' group to gid 800 
+  sudo groupmod -g 800 docker
 else 
   printf 'ZSH_THEME="powerlevel9k/powerlevel9k"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nPOWERLEVEL9K_MODE="nerdfont-complete"\nPOWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable)\nPOWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status vcs virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
 fi
