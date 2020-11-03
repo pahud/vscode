350ace76970fe405bb913033411fcbf788de3a6f
Pahud <pahudnet@gmail.com>
2020-11-03 09:04:10 +0000

    minor

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERNAME=${USERNAME-vscode}
+
+# if in codespaces
+if [[ $CODESPACES == 'true' ]]; then
+  printf 'ZSH_THEME="clean"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
+else 
+  printf 'ZSH_THEME="powerlevel9k/powerlevel9k"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nPOWERLEVEL9K_MODE="nerdfont-complete"\nPOWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable)\nPOWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status vcs virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
+fi
+
+
+echo "exec `which zsh`" > "/home/$USERNAME/.ashrc"
