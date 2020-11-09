a2696c65e880324803fd78127a906c029dbcf153
Pahud <pahudnet@gmail.com>
2020-11-09 08:32:07 +0000

    feat(setup): add is_ec2()

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,12 @@
 
 USERNAME=${USERNAME-vscode}
 
+is_ec2() {
+  curl --max-time 1 -s http://169.254.169.254/latest/meta-data/instance-id 2>&1 > /dev/null
+  return $?
+}
+
+
 # if in codespaces
 if [[ $CODESPACES == 'true' ]]; then
   printf 'ZSH_THEME="clean"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
@@ -9,9 +15,11 @@ if [[ $CODESPACES == 'true' ]]; then
   sudo groupmod -g 800 docker
   # add current user to `docker` group
   sudo usermod -a -G docker $USERNAME
-  # add current user to `root` group
-  sudo usermod -a -G root $USERNAME
 else 
+  if [[ is_ec2 ]]; then
+    # add current user to `root` group
+    sudo usermod -a -G root $USERNAME
+  fi
   printf 'ZSH_THEME="powerlevel9k/powerlevel9k"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nPOWERLEVEL9K_MODE="nerdfont-complete"\nPOWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable)\nPOWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status vcs virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
 fi
 
