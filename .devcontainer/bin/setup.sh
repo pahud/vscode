dd7c9aaab4a5e37032d1e7be54f684a6003ef0ce
Pahud Hsieh <pahudnet@gmail.com>
2021-02-05 12:15:19 +0800

    Update setup.sh

--- .devcontainer/bin/setup.sh
+++ .devcontainer/bin/setup.sh
@@ -2,13 +2,13 @@
 
 USERNAME=${USERNAME-vscode}
 
-# change 'docker' group to gid 992
-sudo groupmod -g 992 docker
-# add current user to `docker` group
-sudo usermod -a -G docker $USERNAME
-newgrp docker
+# # change 'docker' group to gid 992
+# sudo groupmod -g 992 docker
+# # add current user to `docker` group
+# sudo usermod -a -G docker $USERNAME
+# newgrp docker
 
-exit 0
+# exit 0
 
 is_ec2() {
   curl --max-time 1 -s http://169.254.169.254/latest/meta-data/instance-id 2>&1 > /dev/null
@@ -26,13 +26,14 @@ if [[ $CODESPACES == 'true' ]]; then
   newgrp docker
 else 
   if [[ is_ec2 ]]; then
+    echo "[INFO] ec2 instance detected"
     # change 'docker' group to gid 992
     sudo groupmod -g 992 docker
     # add current user to `docker` group
     sudo usermod -a -G docker $USERNAME
     newgrp docker
     # add current user to `root` group
-    sudo usermod -a -G root $USERNAME
+#     sudo usermod -a -G root $USERNAME
   fi
   printf 'ZSH_THEME="powerlevel9k/powerlevel9k"\nENABLE_CORRECTION="false"\nplugins=(git virtualenv)\nPOWERLEVEL9K_MODE="nerdfont-complete"\nPOWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir dir_writable)\nPOWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status vcs virtualenv)\nsource $ZSH/oh-my-zsh.sh' > "/home/$USERNAME/.zshrc"
 fi
